92493a4b69257e803c9fe3aa40493773b2eaf0a8
Stanley <sliang5785@bths.edu>
2017-10-01 14:29:10 -0400

    Finished

--- ControlStructures.java
+++ ControlStructures.java
@@ -1,33 +1,69 @@
-
+// Stanley Liang Lab 1.1 Due 10/1/17
 public class ControlStructures 
 {
-	public static boolean isPrime(int x, int num) 
+	public static boolean isPrime() 
 	{
-		for(x=2; 2*x<=num; x++)
+		int num = 229;
+		for(int x=2; 2*x<=num; x++)
 	{
 		if(num%x==0)
-			{return false;}
-		else
-			{return true;}
-	} 
-		return true;
-	} 
-	public static boolean fBB(int a, boolean foo, boolean Bar, boolean Baz, boolean fooBar, boolean fooBaz, boolean BarBaz, boolean fooBarBaz)
+		{return false;} 
+		
+	} 	return true;
+		
+	}
+	public static void fooBarBaz()
+	{	
+		for(int x=1; x<=500; x++)
+		{
+		if(x%3==0)
+			System.out.print((" "+"foo"));
+		else if(x%7==0)
+			System.out.print((" "+"Bar"));
+		else if(x%10==0)
+			System.out.print((" "+"Baz"));
+		else if(x%3==0 && (x%7==0))
+			System.out.print((" "+"fooBar"));
+		else if(x%3==0 && (x%10==0))
+			System.out.print((" "+"fooBaz"));
+		else if(x%7==0 && (x%10==0))
+			System.out.print((" "+"BarBaz"));
+		else if(x%3==0 && (x%7==0) && (x%10==0))
+			System.out.print((" "+"fooBarBaz"));
+		else {
+			System.out.print(" " + x);
+		}
+	}
+}
+	
+	public static void primePrinter() 
 	{
-		if(a%3==0)
-		{return foo;}
-		else if(a%7==0)
-		{return Bar;}
-		else if(a%10==0)
-		{return Baz;}
-		else if(a%3==0 && (a%7==0))
-		{return fooBar;}
-		else if(a%3==0 && (a%10==0))
-		{return fooBaz;}
-		else if(a%7==0 && (a%10==0))
-		{return BarBaz;}
-		else if(a%3==0 && (a%7==0) && (a%10==0))
-		{return fooBarBaz;}
-		return true;
-	} 
+ 		int x = 0;
+ 		int number = 0;
+ 		int count=0;
+ 		String primeNum = "";
+ 		for (x = 1;  x <= 229; x++) {
+ 			int counter=0;
+ 			for (number=x; number>=1; number--) {
+ 			
+ 			if (x % number == 0) {
+ 				counter = counter +1;
+ 				}
+ 			}
+ 			if (counter==2) {
+ 				System.out.print(" " +x);
+ 				count ++;
+ 			if (count==10) {
+ 				System.out.print("\n");
+ 				count =0;
+ 			}
+ 			primeNum = primeNum + x + " ";
+ 			}
+ 		}
+ 	}
+	public static void main(String[] args)
+	{
+		primePrinter();
+		fooBarBaz();
+	}
 }
